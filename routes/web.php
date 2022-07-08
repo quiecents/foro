4713183643381cf9cf76cb2c43c979fdab7fea9f
Quiecents <quiecents@gmail.com>
2022-07-09 00:55:38 +0200

    verificacion por email de los usuarios registrados

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,33 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+# ==================================================================================== #
+#  Email Verification Routes                                                           #
+# ==================================================================================== #
+// The Email Verification Notice
+Route::get('/email/verify', function () {
+    return view('auth.verify');
+})->middleware('auth')->name('verification.notice');
+
+// The Email Verification Handler
+Route::get('/email/verify/{id}/{hash}', function (Illuminate\Foundation\Auth\EmailVerificationRequest $request) {
+    $request->fulfill();
+ 
+    return redirect('/home');
+})->middleware(['auth', 'signed'])->name('verification.verify');
+
+// Resending The Verification Email
+Route::post('/email/verification-notification', function (Illuminate\Http\Request $request) {
+    $request->user()->sendEmailVerificationNotification();
+ 
+    return back()->with('message', 'Verification link sent!');
+})->middleware(['auth', 'throttle:6,1'])->name('verification.resend');
+# ==================================================================================== #
+# End of Email Verification Routes                                                     #
+# ==================================================================================== #
+
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])
+            ->middleware('verified')
+            ->name('home');
